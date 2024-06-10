558efcdaab2a9552ac95fed71e2140c9824a2fb8
AndyGero2022-0505 <andys.geronimo@gmail.com>
2024-06-10 00:35:53 -0400

    Add all project files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
 test('prints Hola Mundo', () => {
-  const output = execSync('node src/index.js').toString();
-  expect(output).toBe('Hola Mundo\n');
+  const output = execSync('node src/index.js').toString().trim();
+  expect(output).toBe('Hola Mundo');
 });
