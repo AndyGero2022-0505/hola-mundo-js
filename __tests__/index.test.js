77099721208a176d98b2427faa352352e78f8aed
AndyGero2022-0505 <andys.geronimo@gmail.com>
2024-06-10 00:38:47 -0400

    Add debug info and fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,5 +2,6 @@ const { execSync } = require('child_process');
 
 test('prints Hola Mundo', () => {
   const output = execSync('node src/index.js').toString().trim();
+  console.log(`Output: "${output}"`); 
   expect(output).toBe('Hola Mundo');
 });
